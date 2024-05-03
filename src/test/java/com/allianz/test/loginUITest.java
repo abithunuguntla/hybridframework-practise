e3e0d7f759564502c6ceec5833d12431b9f15cb9
abithunuguntla <abi.thunuguntla@gmail.com>
2024-05-03 02:50:45 -0700

    assertion test

--- src/test/java/com/allianz/test/loginUITest.java
+++ src/test/java/com/allianz/test/loginUITest.java
@@ -1,16 +1,38 @@
 package com.allianz.test;
 
+import java.time.Duration;
+
+import javax.xml.xpath.XPath;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class loginUITest {
 	@Test
 	public void titletes() {
-		System.out.println("title test");
-		
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+		driver.get("https://www.open-emr.org/demo/");
+		String actualtitle = driver.getTitle();
+		Assert.assertEquals(actualtitle, "OpenEMR Demo");
+
 	}
+
 	@Test
 	public void Application() {
-		System.out.println("it will execute this method checking ");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+		driver.get("https://opensource-demo.orangehrmlive.com/");
+		String Actualtext = driver.findElement(By.xpath("//*[text()='OrangeHRM OS 5.6.1']")).getText();
+
+		Assert.assertEquals(Actualtext, "OrangeHRM OS 5.6.1");
+
 	}
 
 }
