3a1778b01f33a85a491a296c185cb82409814a66
abithunuguntla <abi.thunuguntla@gmail.com>
2024-05-02 23:22:40 -0700

    created 2 TestNG methods

---
+++ src/test/java/com/allianz/test/loginUITest.java
@@ -0,0 +1,16 @@
+package com.allianz.test;
+
+import org.testng.annotations.Test;
+
+public class loginUITest {
+	@Test(priority = 1)
+	public void titletes() {
+		System.out.println("title test");
+		
+	}
+	@Test(priority = 2)
+	public void Application() {
+		System.out.println("it will execute this method ");
+	}
+
+}
